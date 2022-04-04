5e8056a377176853d8cf19b3ab74ec56419f6b5d
Roy Kim <48267084+foundroy@users.noreply.github.com>
2022-04-03 22:36:06 -0400

    Create ec2-ansible.tf

---
+++ Part-2/ec2-ansible.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "ec2_instance" "part-2" {
+  ami                         = "ami-04505e74c0741db8d"
+  subnet_id                   = "subnet-0929ec2cae1e41a49"
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  security_group              = [aws_security_group.server.id]
+  key_name                    = "labkey"
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(local.private_key_path)
+    host        = "ec2_instance.server.public_ip"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+    "sudo apt-get update",
+    "sudo apt-add-repository -y"
+    "sudo apt-get update"
+    "sudo apt-get install -y ansible"
+    "sudo apt install python3-pip -y"
+    "sudo pip install boto boto3"
+    "sudo apt-get install python3-boto -y"
+    "ansible-galaxy collection install amazon.aws"
+    "sudo "[localhost]" >> /etc/ansible/hosts"
+    "sudo "local" >> /etc/ansible/hosts"
+    ]
+  }
+}
